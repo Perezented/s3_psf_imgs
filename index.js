4f060ee2a7df852ce9cb05746d581dc4410065f7
Michael Perez <mperez6473@gmail.com>
2020-11-23 13:33:20 -0600

    a /upload endpoint for get

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const port = 3025;
 
+app.get("/upload", (req, res) => {
+  res.send({
+    message:
+      "Hello World! This is a posting link. You may want to post image to this endpoint instead."
+  });
+});
+
 app.listen(port, () => {
   console.log(`server is up at http://localhost:${port}
   `);
